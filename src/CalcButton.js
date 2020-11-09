ea40eafd183ed517df4ed478ac78fceaf7b7f3f9
Ilan Prais <ilandovprais@gmail.com>
2020-11-09 22:01:08 +0200

    deleting uneccesary files

--- src/CalcButton.js
+++ src/CalcButton.js
@@ -3,10 +3,9 @@ import {Component} from 'react'
 class CalcButton extends Component {
 
     render(){
-        let s = {marginLeft: this.props.xpos, marginTop: this.props.ypos};
         let text = this.props.text;
         return (
-            <button style = {s} onClick={this.props.onClick}>{text}</button>
+            <button onClick={this.props.onClick}>{text}</button>
         );
     }
 }
